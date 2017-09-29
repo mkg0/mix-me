b9e1d415ac5c14f5f13c583ece931de0a30bcb5a
HermanDahlen <hman.dahlen@gmail.com>
2017-09-29 11:48:16 +0200

    First version of the login screen

--- client/src/Register.js
+++ client/src/Register.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./App.css";
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="App">
+      <h1>Mix Me!</h1>
+      <p>Find buddies to have lunch with.</p>
+      <input type="text" placeholder="firstname.lastname" />
+      <button>Sign me up!</button>
+    </div>
+  );
 }
