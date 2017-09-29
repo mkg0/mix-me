0ef0272571e3aacf08c4c2810842db2b8c66c779
HermanDahlen <hman.dahlen@gmail.com>
2017-09-29 18:05:15 +0200

    add registered state

--- client/src/Register.js
+++ client/src/Register.js
@@ -19,43 +19,42 @@ const styles = {
   },
 }
 
-function Register({ onChange, onSubmit, toggleTutorial, tutorial }) {
+function Register({ onChange, onSubmit, registered }) {
   return (
     <div className="register-page">
       <h1 className="register-logo">🍔 MixMe</h1>
       <div className="register-card">
-        <p className="register-description">Find buddies to have lunch with</p>
-
-        <div className="register-signup">
-          <TextField
-            id="register-input"
-            className="register-input"
-            onChange={onChange}
-            hintText="Your mail"
-          />
-          <span className="register-signavio">@signavio.com</span>
-        </div>
-        <div>
-          <RaisedButton label="Match me today!" primary onClick={onSubmit} />
-          <div
-            style={{
-              color: 'rgba(0, 0, 0, 0.3)',
-              fontSize: 16,
-              fontWeight: 300,
-              marginTop: 120,
-            }}
-          >
-            <span>New here? </span>
-            <Link
-              to="/tutorial"
-              style={{
-                textDecoration: 'underline',
-                color: 'rgba(0, 0, 0, 0.3)',
-              }}
-            >
-              Take the tutorial
-            </Link>
+        <p className="register-description">
+          Find buddies to have lunch with today
+        </p>
+        {registered ? (
+          <div>
+            {' '}
+            <p>You are signed up!</p>
+            <p>We'll find a match for as quick as possible!</p>
           </div>
+        ) : (
+          <div className="register-signup">
+            <TextField
+              id="register-input"
+              className="register-input"
+              onChange={onChange}
+              hintText="Your mail"
+            />
+            <span className="register-signavio">@signavio.com</span>
+            <RaisedButton
+              label="Match me!"
+              className="register-button"
+              primary
+              onClick={onSubmit}
+            />
+          </div>
+        )}
+        <div className="register-tutorial">
+          <span>New here? </span>
+          <Link to="/tutorial" className="register-tutorial-link">
+            Take the tutorial
+          </Link>
         </div>
       </div>
     </div>
@@ -63,17 +62,15 @@ function Register({ onChange, onSubmit, toggleTutorial, tutorial }) {
 }
 
 export default compose(
-  withState('tutorial', 'setTutorial', false),
+  withState('registered', 'setRegistered', false),
   withState('username', 'setUsername', null),
   withHandlers({
     onChange: ({ setUsername }) => event => {
       setUsername(event.target.value)
     },
-    onSubmit: ({ username }) => event => {
+    onSubmit: ({ username, setRegistered }) => event => {
+      setRegistered(true)
       console.log(username)
-    },
-    toggleTutorial: ({ setTutorial, tutorial }) => () => {
-      setTutorial(!tutorial)
     },
   })
 )(Register)
