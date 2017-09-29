966fe6f5549a17e8046e653703febf011f1f304a
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2017-09-29 15:56:16 +0200

    Add steps and modify some text

--- client/src/Tutorial.js
+++ client/src/Tutorial.js
@@ -2,48 +2,66 @@ import React from 'react'
 import { withState, withHandlers, compose } from 'recompose'
 
 import {
+  AppBar,
   Step,
   Stepper,
   StepButton,
   StepContent,
   RaisedButton,
   FlatButton,
+  Subheader,
 } from 'material-ui'
 
 function Tutorial({ step, onNext, onPrevious, onSelectStep }) {
   return (
-    <Stepper activeStep={step} linear={false} orientation="vertical">
-      <Step>
-        <StepButton onClick={() => onSelectStep(0)}>
-          Select campaign settings
-        </StepButton>
-        <StepContent>
-          <p>
-            For each ad campaign that you create, you can control how much
-            you're willing to spend on clicks and conversions, which networks
-            and geographical locations you want your ads to show on, and more.
-          </p>
+    <div>
+      <AppBar showMenuIconButton={false} title="Tutorial" />
 
-          <RaisedButton primary label="Next" onClick={onNext} />
-        </StepContent>
-      </Step>
+      <Subheader>
+        Mix and Match - a way to get to know your colleagues
+      </Subheader>
+      <Stepper activeStep={step} linear={false} orientation="vertical">
+        <Step>
+          <StepButton onClick={() => onSelectStep(0)}>Sign Up</StepButton>
+          <StepContent>
+            <p>
+              better you're willing to spend on clicks and conversions, which
+              networks and geographical locations you want your ads to show on,
+              and more.
+            </p>
+            <RaisedButton primary label="Next" onClick={onNext} />
+          </StepContent>
+        </Step>
 
-      <Step>
-        <StepButton onClick={() => onSelectStep(1)}>
-          Select campaign settings
-        </StepButton>
-        <StepContent>
-          <p>
-            For each ad campaign that you create, you can control how much
-            you're willing to spend on clicks and conversions, which networks
-            and geographical locations you want your ads to show on, and more.
-          </p>
+        <Step>
+          <StepButton onClick={() => onSelectStep(1)}>
+            Get a notification with your buddies group
+          </StepButton>
+          <StepContent>
+            <p>
+              For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.
+            </p>
+            <RaisedButton primary label="Next" onClick={onNext} />
+            <FlatButton label="Previous" onClick={onPrevious} />
+          </StepContent>
+        </Step>
 
-          <RaisedButton primary label="Next" onClick={onNext} />
-          <FlatButton label="Previous" onClick={onPrevious} />
-        </StepContent>
-      </Step>
-    </Stepper>
+        <Step>
+          <StepButton onClick={() => onSelectStep(2)}>
+            Meet and have fun!
+          </StepButton>
+          <StepContent>
+            <p>
+              For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.
+            </p>
+          </StepContent>
+        </Step>
+      </Stepper>
+    </div>
   )
 }
 
