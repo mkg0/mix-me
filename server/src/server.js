3f00e292cf3cc036ef9bf8b0298c96c63fc2febd
Julius Breckel <julius.breckel@signavio.com>
2017-09-29 17:01:01 +0200

    fix server

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,28 @@
 import { join } from 'path'
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 
-app.get('/api', (req, res) => {
+const people = []
+
+app.get('/api/didimatch', (req, res) => {
     res.send({
-        message: 'I am a server route and can also be hot reloaded!',
+        names: people.map(({ name }) => name),
+        location: 'Lounge',
     })
 })
 
-app.get('*', express.static(join(__dirname, 'public')))
+app.post('/api/matchme', bodyParser.json(), (req, res) => {
+    people.push(req.body)
+    res.status(204).send()
+})
+
+// Serve static files from the React app
+app.use('/static', express.static(join(__dirname, '/public/static')))
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '/public/index.html'))
+})
 
 export default app
