71a5d764169a77bf61a27d53190faa2f21d31427
Iyad Al-Sahwi <iyad.al-sahwi@signavio.com>
2017-09-29 16:34:05 +0200

    create groups of each 5 persons

--- server/src/mixRandom.js
+++ server/src/mixRandom.js
@@ -3,20 +3,47 @@
  */
 
 
-//Cache
-var alreadyMatchedNames = []
+const names = ['iyad.al', 'julius.b', 'jonas.h', 'alexander', 'hermann', 'phil.g', 'newGuy', 'another.one']
+//const names = ['iyad.al', 'julius.b', 'jonas.h', 'alexander', 'hermann', 'phil.g', 'newGuy']
+console.log('name length ', names.length)
 
-var names = ['iyad.al', 'julius.b', 'jonas.h', 'alexander', 'hermann', 'phil.g', 'newGuy', 'another.one']
 
 console.log('before', names)
 
-var groups = shuffle(names).map((name, index)=> {
-    return ({name, group: index%5+1})
-})
+var shuffledNames = shuffle(names)
+var groups = shuffledNames.reduce((groupedNames, name, index) => {
+    //const pos = index%5+1
+    const pos = Math.floor(index/5)
+        if(!groupedNames[pos]) {
+            groupedNames[pos] = []
+    }
+    groupedNames[pos].push(name)
+    return groupedNames
+}, {})
 
 console.log('after', names)
 console.log('group', groups)
 
+if(names.length > 5) {
+    const lastGroup = groups[Object.keys(groups).length - 1]
+    if (lastGroup.length < 3) {
+        console.log(lastGroup.length + ' is small than 3')
+        const secondLastGroup = groups[Object.keys(groups).length - 2]
+        let lastFromNames = secondLastGroup[secondLastGroup.length - 1]
+        let secondLastFromNames = secondLastGroup[secondLastGroup.length - 2]
+        console.log(lastFromNames + ' is last guy')
+        console.log(secondLastFromNames + ' is secondLast guy')
+        secondLastGroup.pop(lastFromNames)
+        secondLastGroup.pop(secondLastFromNames)
+        lastGroup.push(lastFromNames)
+        lastGroup.push(secondLastFromNames)
+    }
+}
+
+console.log('as array', Object.keys(groups).map(key => groups[key]).map((namesForGroup) => ({ names: namesForGroup })))
+
+
+// from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
