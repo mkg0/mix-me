13ba5f98dc686be9badd718717dd10315714143d
HermanDahlen <hman.dahlen@gmail.com>
2017-09-29 11:59:15 +0200

    Registration page styling

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import './App.css'
+import './Register.css'
 
 export default function Register() {
   return (
-    <div className="App">
-      <h1>Mix Me!</h1>
-      <p>Find buddies to have lunch with.</p>
-      <input type="text" placeholder="firstname.lastname" />
-      <button>Sign me up!</button>
+    <div className="register-page">
+      <h1 className="register-logo">🍔 Mix Me! 🍔</h1>
+      <p className="register-description">Find buddies to have lunch with.</p>
+      <div className="register-signup">
+        <input
+          className="register-input"
+          type="text"
+          placeholder="firstname.lastname"
+        />
+        <span>@signavio.com</span>
+      </div>
+      <button className="register-button">Sign me up!</button>
     </div>
   )
 }
