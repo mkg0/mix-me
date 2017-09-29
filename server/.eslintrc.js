e6afbaa81faf7ea9ae96185ab54cdb63dff1d34e
Julius Breckel <julius.breckel@signavio.com>
2017-09-29 11:46:14 +0200

    init server

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: 'signavio',
+    parser: 'babel-eslint',
+    globals: {
+        log: true,
+        info: true,
+        debug: true,
+        warn: true,
+        error: true,
+    },
+    rules: {
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: ['**/test/**', '**/tools/**'],
+            },
+        ],
+    },
+}
