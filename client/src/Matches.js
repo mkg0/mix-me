c58264638396ad665f2f2f0c991448934ad19984
Philipp Giese <giese.philipp+git@gmail.com>
2017-09-29 14:48:02 +0200

    added material ui for match page

--- client/src/Matches.js
+++ client/src/Matches.js
@@ -1,14 +1,57 @@
 import React from 'react'
+import { capitalize } from 'lodash'
+
+import Subheader from 'material-ui/Subheader'
+import { List, ListItem } from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import { Card, CardHeader } from 'material-ui/Card'
+import AppBar from 'material-ui/AppBar'
+import Divider from 'material-ui/Divider'
+
+import { pink500 } from 'material-ui/styles/colors'
+
+import { Room } from 'material-ui-icons'
 
 export default function Matches({ matches }) {
   return (
     <div>
-      <p>There is a match!</p>
-      <h1>Your group for today</h1>
-      <ul>{matches.names.map(({ name }) => <li key={name}>{name}</li>)}</ul>
-      You'll meet here:
-      <br />
-      <strong>{matches.location}</strong>
+      <AppBar showMenuIconButton={false} title="There is a match!" />
+
+      <Subheader>Your group for today</Subheader>
+
+      <List>
+        {matches.names.map(({ name }) => (
+          <ListItem
+            disabled
+            key={name}
+            leftAvatar={<Avatar>{initials(name)}</Avatar>}
+            primaryText={fullName(name)}
+          />
+        ))}
+      </List>
+
+      <Divider />
+
+      <List>
+        <Subheader inset>We will meet at...</Subheader>
+
+        <ListItem
+          leftAvatar={<Avatar icon={<Room />} backgroundColor={pink500} />}
+          primaryText={matches.location}
+        />
+      </List>
     </div>
   )
+}
+
+const fullName = name => {
+  const [firstName, lastName] = name.split('.')
+
+  return `${capitalize(firstName)} ${capitalize(lastName)}`
+}
+
+const initials = name => {
+  const [firstName, lastName] = name.split('.')
+
+  return lastName.slice(0, 1).toUpperCase()
 }
