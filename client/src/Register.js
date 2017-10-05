27b12262c68c9725504cf4117c4608197db2d58b
Jonas <jonas.hartweg@signavio.com>
2017-10-05 12:09:40 +0200

    ask for permission after registration

--- client/src/Register.js
+++ client/src/Register.js
@@ -83,6 +83,23 @@ export default compose(
         registerUser(username).then(() => {
           setRegistered(true)
           Cookies.set('name', username)
+
+          // Let's check if the browser supports notifications
+          if (!('Notification' in window)) {
+            alert('This browser does not support desktop notification')
+          } else if (Notification.permission === 'granted') {
+            // Let's check whether notification permissions have already been granted
+            // If it's okay let's create a notification
+            console.log('Notification access already granted')
+          } else if (Notification.permission !== 'denied') {
+            // Otherwise, we need to ask the user for permission
+            Notification.requestPermission(function(permission) {
+              // If the user accepts, let's create a notification
+              if (permission === 'granted') {
+                console.log('Notification access granted')
+              }
+            })
+          }
         })
       } catch (error) {
         setError(error)
