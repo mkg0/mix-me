632045a09c34ab08a4b8d9012de1c407f5e4277c
Philipp Giese <giese.philipp+git@gmail.com>
2017-10-05 14:59:53 +0200

    added content type header

--- client/src/api/service.js
+++ client/src/api/service.js
@@ -1,6 +1,9 @@
 export default async function registerUser(name) {
   return await fetch('/api/matchme', {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ name }),
   })
 }
