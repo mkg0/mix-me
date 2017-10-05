bb32008e9e0e0bf0e937b5c4c42bb9ff893b7ff4
Philipp Giese <giese.philipp+git@gmail.com>
2017-10-05 17:16:51 +0200

    mongo config handles non-auth scenario

--- server/src/configMongoDB.js
+++ server/src/configMongoDB.js
@@ -2,20 +2,21 @@ import mongoose from 'mongoose'
 import nconf from 'nconf'
 
 export default () => {
-    const { DB_USER, DB_PASS, DB_HOST } = nconf.get()
-    const MONGO_URI = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/mixme`
+  const { DB_USER, DB_PASS, DB_HOST } = nconf.get()
 
-    if (!MONGO_URI) {
-        throw new Error('You must provide a MongoLab URI')
-    }
+  let MONGO_URI
 
-    mongoose.Promise = global.Promise
-    mongoose.connect(MONGO_URI, { useMongoClient: true })
-    /* eslint-disable no-console */
-    mongoose.connection
-        .once('open', () => console.log('Connected to MongoLab instance.'))
-        .on('error', error =>
-            console.log('Error connecting to MongoLab:', error)
-        )
-    /* eslint-enable no-console */
+  if (!DB_USER || !DB_PASS) {
+    MONGO_URI = `mongodb://${DB_HOST}/mixme`
+  } else {
+    MONGO_URI = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/mixme`
+  }
+
+  mongoose.Promise = global.Promise
+  mongoose.connect(MONGO_URI, { useMongoClient: true })
+  /* eslint-disable no-console */
+  mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error))
+  /* eslint-enable no-console */
 }
