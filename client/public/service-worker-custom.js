f76ee5fd5f0606f50a07cb9a80fc81ac74885a2f
Philipp Giese <giese.philipp+git@gmail.com>
2017-10-05 17:52:15 +0200

    click on notification takes me to match

--- client/public/service-worker-custom.js
+++ client/public/service-worker-custom.js
@@ -1,17 +1,25 @@
-fetch('/api/matchme').then(response => {
-  console.log(response)
+this.addEventListener('activate', () => {
+  setTimeout(() => {
+    this.registration.showNotification("It's a match!", {
+      body: 'We found a Mix & Match group for you',
+      icon: 'hamburger.png',
+      badge: 'hamburger.png',
+    })
+  }, 5000)
 })
 
-this.addEventListener('activate', function(event) {
-  const title = "It's a match!"
-  const options = {
-    body: 'We found a Mix & Match group for you',
-    icon: 'hamburger.png',
-    badge: 'hamburger.png',
-  }
+this.addEventListener('notificationclick', event => {
+  event.waitUntil(
+    this.clients
+      .matchAll({
+        type: 'window',
+      })
+      .then(clients => {
+        clients.forEach(client => {
+          if (client.url == '/' && 'focus' in client) return client.focus()
+        })
 
-  console.log('Scheduled a notification')
-  setTimeout(() => {
-    this.registration.showNotification(title, options)
-  }, 5000)
+        if (this.clients.openWindow) return this.clients.openWindow('/')
+      })
+  )
 })
