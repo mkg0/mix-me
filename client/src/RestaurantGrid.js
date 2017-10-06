d4d1d473a71d9e9f2562bd68fb3a880bf70a4cee
Julius Breckel <julius.breckel@signavio.com>
2017-10-06 14:33:31 +0200

    use https for imgs, shorten urls

--- client/src/RestaurantGrid.js
+++ client/src/RestaurantGrid.js
@@ -14,47 +14,42 @@ const styles = {
 
 const tilesData = [
   {
-    img:
-      'http://www.top10berlin.de/sites/top10berlin.de/files/styles/list_image/public/location/mainimages/2017/02/16/top10berlin_dolores-burritos001.jpg?',
+    img: 'https://tinyurl.com/y998e7ww',
     title: 'Dolores',
     distance: 6,
   },
   {
-    img:
-      'https://www.online-tischreservierung.de/images/restaurants/Berlin/Berlin/ko8ca6d28q/1.jpg',
+    img: 'https://tinyurl.com/yb9qor68',
     title: 'Evin',
     distance: 3,
   },
   {
-    img:
-      'https://media-cdn.tripadvisor.com/media/photo-s/07/60/a0/8e/cafe-backerei.jpg',
+    img: 'https://tinyurl.com/ybtnn6qp',
     title: 'Cafe Bäckerei',
     distance: 3,
   },
   {
-    img: 'https://www.kalus-restaurant.de/images/900/1645878/IMG_0912.JPG',
+    img: 'https://tinyurl.com/yalbh3hw',
     title: 'Kalus',
     distance: 3,
   },
   {
-    img: 'https://media-cdn.tripadvisor.com/media/photo-s/0b/95/94/0b/reva.jpg',
+    img: 'https://tinyurl.com/ybjjfs9a',
     title: 'Reva',
     distance: 3,
   },
   {
-    img: 'https://s3-media4.fl.yelpcdn.com/bphoto/yjckhWlnqPivQtL5GAGt6w/o.jpg',
+    img: 'https://tinyurl.com/y83jz4j8',
     title: 'Pucci',
     distance: 4,
   },
   {
-    img:
-      'https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/13240483_255733911482823_1100800166297608233_n.jpg?oh=677d5b0fde4094a82174b97c27a1b1e8&oe=5A8334BF',
+    img: 'https://tinyurl.com/yaew2n5p',
     title: 'Kimiko',
     distance: 2,
   },
   {
-    img:
-      'https://www.falstaff.de/fileadmin/_processed_/csm_01-Chicago-Steakhouse-beigestellt-2048_e0308280f5.jpg',
+    img: 'https://tinyurl.com/y8qwt9x7',
     title: 'Chicago Steak House',
     distance: 6,
   },
